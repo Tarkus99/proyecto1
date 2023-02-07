4c8dcc924614bfc8c4575434577afb74f1c5153c
genguidanosn <gabengneb@alu.edu.gva.es>
2023-02-07 23:56:54 +0100

    tercer commit clase principal

--- prueba1/src/prueba1/Principal.java
+++ prueba1/src/prueba1/Principal.java
@@ -5,6 +5,7 @@ public class Principal {
 	public static void main(String[] args) {
 		System.out.println("Hola mundo java-git"+1);
 		System.out.println("Hola mundo java-git"+2);
+		System.out.println("Hola mundo java-git"+3);
 
 	}
 
